a7b6461b5068f18af09c9ec38adc7e72b2362056
Dibakala-Muke <dibakala90@gmail.com>
2025-08-12 08:02:54 +0100

    Suppression du lien du flowbite dans header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,5 +3,4 @@
     <title>FIJADA - Forum International de la Jeunesse Africaine</title>
     @vite('resources/css/app.css')
     <script src="https://cdn.jsdelivr.net/npm/@heroicons/react/outline"></script>
-    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
 
