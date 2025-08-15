c7aaee483b97e943fa81b51d89f6f3fb3fa12663
Dibakala-Muke <dibakala90@gmail.com>
2025-08-15 18:03:45 +0100

    change

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,6 +6,17 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 @else
     <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        @theme {
+            --font-sans: "Instrument Sans", ui-sans-serif, system-ui, sans-serif,
+                "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+                "Noto Color Emoji";
+            --color-fijada-green: #30b753;
+            --color-fijada-blue: #0d80ff;
+            --color-fijada-reseau: #4ebd70;
+            --color-fijada-hero: #00000066;
+        }
+    </style>
 @endif
 <script src="https://cdn.jsdelivr.net/npm/@heroicons/react/outline"></script>
 <link rel="icon" href="images/LOGO FIJADA.png" type="image/x-icon">
